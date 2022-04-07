94a0d3787d6885818a80fc627743e66fa3d1d12c
msinkec <mihael@sinkec.xyz>
2022-04-07 11:41:58 +0200

    All tests passing.

--- test.py
+++ test.py
@@ -40,12 +40,12 @@ if __name__ == '__main__':
 
     contract = './secp521r1.scrypt' 
 
-    compiler_result = compile_contract(contract, debug=False)
-    desc = compiler_result.to_desc()
+    #compiler_result = compile_contract(contract, debug=False)
+    #desc = compiler_result.to_desc()
 
     # Load desc instead:
-    #with open('./out/secp521r1_desc.json', 'r') as f:
-    #    desc = json.load(f)
+    with open('./out/secp521r1_desc.json', 'r') as f:
+        desc = json.load(f)
 
     type_classes = build_type_classes(desc)
     Point = type_classes['Point']
@@ -61,103 +61,50 @@ if __name__ == '__main__':
                 Point({ 'x': point_sum.x(), 'y': point_sum.y()}), 
             ).verify()
 
-    ## Point doubling
-    #dx, dy = point_doubled.to_point()
+    # Point doubling
+    assert testCheckSig.testDouble(
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+                Point({ 'x': point_doubled.x(), 'y': point_doubled.y()}), 
+            ).verify()
 
-    #assert testCheckSig.testDouble(
-    #            Point({ 'x': ax, 'y': ay}), 
-    #            Point({ 'x': dx, 'y': dy}), 
-    #        ).verify()
-
-    ## Point doubling, point at inf
-    #assert testCheckSig.testDouble(
-    #            Point({ 'x': 0, 'y': 0}), 
-    #            Point({ 'x': 0, 'y': 0}), 
-    #        ).verify()
+    # Point doubling, point at inf
+    assert testCheckSig.testDouble(
+                Point({ 'x': 0, 'y': 0}), 
+                Point({ 'x': 0, 'y': 0}), 
+            ).verify()
 
 
-    ## Point addition, same point
-    #assert testCheckSig.testAdd(
-    #            Point({ 'x': ax, 'y': ay}), 
-    #            Point({ 'x': ax, 'y': ay}), 
-    #            Point({ 'x': dx, 'y': dy}), 
-    #        ).verify()
+    # Point addition, same point
+    assert testCheckSig.testAdd(
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+                Point({ 'x': point_doubled.x(), 'y': point_doubled.y()}), 
+            ).verify()
 
-    ## Point addition, point at inf
-    #assert testCheckSig.testAdd(
-    #            Point({ 'x': 0, 'y': 0}), 
-    #            Point({ 'x': bx, 'y': by}), 
-    #            Point({ 'x': bx, 'y': by}), 
-    #        ).verify()
-    #assert testCheckSig.testAdd(
-    #            Point({ 'x': ax, 'y': ay}), 
-    #            Point({ 'x': 0, 'y': 0}), 
-    #            Point({ 'x': ax, 'y': ay}), 
-    #        ).verify()
+    # Point addition, point at inf
+    assert testCheckSig.testAdd(
+                Point({ 'x': 0, 'y': 0}), 
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+            ).verify()
+    assert testCheckSig.testAdd(
+                Point({ 'x': point_doubled.x(), 'y': point_doubled.y()}), 
+                Point({ 'x': 0, 'y': 0}), 
+                Point({ 'x': point_doubled.x(), 'y': point_doubled.y()}), 
+            ).verify()
 
 
-    ## Scalar multiplication
-    #prodx, prody = point_scaled.to_point()
-    #assert testCheckSig.testMultByScalar(
-    #            Point({ 'x': ax, 'y': ay}), 
-    #            scalar.to_int(), 
-    #            Point({ 'x': prodx, 'y': prody}), 
-    #        ).verify()
+    # Scalar multiplication
+    assert testCheckSig.testMultByScalar(
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+                scalar, 
+                Point({ 'x': point_scaled.x(), 'y': point_scaled.y()}), 
+            ).verify()
 
-    ## Signature verification
-    #assert testCheckSig.testVerifySig(
-    #            msg_bytes,
-    #            Signature({ 'r': r, 's': s}), 
-    #            Point({ 'x': ax, 'y': ay}), 
-    #        ).verify()
-
-    # Point addition with many random keys
-    #for i in range(500):
-    #    print("Adding rand key, iter. {}".format(i))
-    #    rand_key_priv = PrivateKey.from_random()
-    #    rand_to_add = PrivateKey.from_random()
-    #    rand_point_sum = rand_key_priv.public_key.add(rand_to_add._secret)
-
-    #    rax, ray = rand_key_priv.public_key.to_point()
-    #    rbx, rby = rand_to_add.public_key.to_point()
-    #    rsumx, rsumy = rand_point_sum.to_point()
-
-    #    assert testCheckSig.testAdd(
-    #                Point({ 'x': rax, 'y': ray}), 
-    #                Point({ 'x': rbx, 'y': rby}), 
-    #                Point({ 'x': rsumx, 'y': rsumy}), 
-    #            ).verify()
-
-    ## Point double with many random keys
-    #for i in range(500):
-    #    print("Doubling rand key, iter. {}".format(i))
-    #    rand_key_priv = PrivateKey.from_random()
-    #    rand_point_sum = rand_key_priv.public_key.add(rand_key_priv._secret)
-
-    #    rax, ray = rand_key_priv.public_key.to_point()
-    #    rsumx, rsumy = rand_point_sum.to_point()
-
-    #    assert testCheckSig.testDouble(
-    #                Point({ 'x': rax, 'y': ray}), 
-    #                Point({ 'x': rsumx, 'y': rsumy}), 
-    #            ).verify()
-
-    # Scalar point multiplication with many random keys
-    #for i in range(100):
-    #    print("Multiplying rand key and scalar, iter. {}".format(i))
-    #    scalar = PrivateKey.from_random()
-
-    #    pub_key = PrivateKey.from_random().public_key
-    #    ax, ay = pub_key.to_point()
-    #    
-    #    prod = pub_key.multiply(scalar._secret)
-    #    prodx, prody = prod.to_point()
-
-    #    assert testCheckSig.testMultByScalar(
-    #                Point({ 'x': ax, 'y': ay}), 
-    #                scalar.to_int(), 
-    #                Point({ 'x': prodx, 'y': prody}), 
-    #            ).verify()
-        
-
+    # Signature verification
+    assert testCheckSig.testVerifySig(
+                msg_bytes,
+                Signature({ 'r': r, 's': s}), 
+                Point({ 'x': point_pub.x(), 'y': point_pub.y()}), 
+            ).verify()
 
